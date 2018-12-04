8c62a286e3e936873ff2cf2597cf985ba5a0d783
Aga <a.dufrat@gmail.com>
2018-12-04 16:55:39 +0000

    Addressed comments from review

--- app.rb
+++ app.rb
@@ -18,11 +18,10 @@ class Airbnb < Sinatra::Base
   end
 
   post '/users' do
-    user = User.create(
-      name: params[:name],
-      username: params[:username],
-      email: params[:email],
-      password: params[:password])
+    user = User.create(name: params[:name],
+                       username: params[:username],
+                       email: params[:email],
+                       password: params[:password])
     session[:user_id] = user.id
     redirect '/'
   end
