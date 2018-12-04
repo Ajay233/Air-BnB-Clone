d4536a41036989c57da766d47fa8c97998435213
Karen Roberts <karenroberts@Karens-MacBook-Pro.local>
2018-12-04 13:51:07 +0000

    added bcrypt and build_user method to User model

--- lib/models/user.rb
+++ lib/models/user.rb
@@ -1,15 +1,22 @@
 require 'pg'
+require 'bcrypt'
 require_relative '../database_connection'
 
 class User
 
   def self.create(name:, username:, email:, password:)
+    encrypted_password = BCrypt::Password.create(password)
+
      result = DatabaseConnection.query("INSERT INTO users (name, username, email, password) \
-                              VALUES('#{name}', '#{username}', '#{email}', '#{password}') \
+                              VALUES('#{name}', '#{username}', '#{email}', '#{encrypted_password}') \
                               RETURNING id, name, username, email;").first
 
-     User.new(id: result['id'], name: result['name'],
-              username: result['username'], email: result['email'])
+     build_user(result)
+  end
+
+  def self.build_user(result)
+    User.new(id: result['id'], name: result['name'],
+             username: result['username'], email: result['email'])
   end
 
   attr_accessor :id, :name, :username, :email
