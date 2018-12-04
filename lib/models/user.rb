e80e0aa718e58b5c66f81e9d36627614fbfb6bfa
Karen Roberts <karenroberts@Karens-MacBook-Pro.local>
2018-12-04 11:44:39 +0000

    added initialize to User model with instance variables

--- lib/models/user.rb
+++ lib/models/user.rb
@@ -1,2 +1,10 @@
 class User
+  attr_accessor :id, :name, :username, :email
+
+  def initialize(id:, name:, username:, email:)
+    @id = id
+    @name = name
+    @username = username
+    @email = email
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,18 @@
 require_relative '../../lib/models/user'
 
 describe User do
-  let(:user) { User.new }
+  let(:user) { User.new(id: "1", name: "Name1", username: "Username", email: "email@email.com") }
 
   it "is an instance of User" do
     expect(user).to be_an_instance_of User
+  end
+
+  describe "initialize" do
+    it "has id, name, username, email" do
+      expect(user.id).to eq('1')
+      expect(user.name).to eq('Name1')
+      expect(user.username).to eq('Username')
+      expect(user.email).to eq('email@email.com')
+    end
   end
 end
