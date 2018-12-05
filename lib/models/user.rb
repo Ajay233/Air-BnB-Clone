2c3d109aa030682e283f053e8501f5d3ca9c0bb4
willmcb <williamjamesmcbrien@gmail.com>
2018-12-05 16:16:40 +0000

    A user can now look up the spaces that they own

--- lib/models/user.rb
+++ lib/models/user.rb
@@ -55,4 +55,9 @@ class User
     @username = username
     @email = email
   end
+
+  def spaces
+    results = DatabaseConnection.query("SELECT * FROM spaces WHERE owner_id = #{id};")
+    results.map { |result| Space.build_space(result) }
+  end
 end
